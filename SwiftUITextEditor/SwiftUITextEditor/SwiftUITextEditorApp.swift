b20b606b8b201c668a1ceadb4dd443411900f5e5
Andreas Velounias <>
2021-03-29 14:19:59 +0100

    init

---
+++ SwiftUITextEditor/SwiftUITextEditor/SwiftUITextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextEditorApp.swift
+//  SwiftUITextEditor
+//
+//  Created by Andreas Velounias on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
